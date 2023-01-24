4e57b55b9cf004c3f0ef89c2794e5dfc81e8a32e
najjarpannu <53272227+najjarpannu@users.noreply.github.com>
2023-01-23 19:48:54 -0500

    final

--- RidesTests/RidesTests.swift
+++ RidesTests/RidesTests.swift
@@ -9,6 +9,7 @@ import XCTest
 @testable import Rides
 
 final class RidesTests: XCTestCase {
+   
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
